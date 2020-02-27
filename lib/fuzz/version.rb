3b6d0dccc79430287f02990414c247c04c338aae
Johnny Willemsen <jwillemsen@remedy.nl>
2020-02-27 12:10:11 +0100

    Incremented version
        * lib/fuzz/version.rb:

--- lib/fuzz/version.rb
+++ lib/fuzz/version.rb
@@ -11,7 +11,7 @@ module Fuzz
 
   FUZZ_VERSION_MAJOR = 0.freeze
   FUZZ_VERSION_MINOR = 9.freeze
-  FUZZ_VERSION_RELEASE = 6.freeze
+  FUZZ_VERSION_RELEASE = 7.freeze
   FUZZ_VERSION = "#{FUZZ_VERSION_MAJOR}.#{FUZZ_VERSION_MINOR}.#{FUZZ_VERSION_RELEASE}"
   FUZZ_COPYRIGHT = "Copyright (c) 2012-#{Time.now.year} Remedy IT Expertise BV, The Netherlands".freeze
 
